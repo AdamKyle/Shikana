dc9f9a8e6d7374600e90043d2b4032c10cd6fef2
Adam Balan <a-kyle@hotmail.com>
2018-02-16 22:54:16 -0700

    Added DiscardPiletest

---
+++ Shikana.Game.Logic.Test/Game/DiscardPileTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Shikana.Cards;
+using Shikana.Game.Logic.Game;
+
+namespace Shikana.Game.Logic.Test.Game
+{
+    [TestFixture]
+    class DiscardPileTest
+    {
+        [Test]
+        public void addCardsToDiscardPile()
+        {
+            Deck deck = new Deck();
+            var createdDeck = deck.createDeck();
+
+            DiscardPile dp = new DiscardPile();
+            dp.addCardsToDiscardPile(createdDeck);
+
+            Assert.AreNotEqual(0, dp.DiscardedCards.Count);
+        }
+    }
+}
